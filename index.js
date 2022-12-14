4c69983cb6973b1f86772b21fc3daa347b258fd9
dekta <ektadhawal6306@gmail.com>
2022-12-15 00:37:31 +0530

    changes

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const cors = require("cors");
 
 const {connect} = require("./config/db")
 const {AdminModel} = require("./models/admin.model")
-const {authenticate} = require("./middleware/authentication")
+const {authenticate_admin,authenticate_user} = require("./middleware/authentication")
 const {hotelRouter} = require("./routes/hotel.route")
 const {HotelModel} = require("./models/hotel.model")
+const {UserModel} = require("./models/user.model")
 
 app.use(express.json());
 app.use(cors({
@@ -25,7 +26,7 @@ app.get("/hotels",async(req,res)=>{
     res.send(data)
 })
 
-app.post("/signup", async(req,res)=>{
+app.post("/admin/signup", async(req,res)=>{
     const {name,email,password,isadmin} = req.body;
     const userpresent = await AdminModel.findOne({email})
 
@@ -47,7 +48,7 @@ app.post("/signup", async(req,res)=>{
 
 })
 
-app.post("/login",async(req,res)=>{
+app.post("/admin/login",async(req,res)=>{
     const {email,password}=req.body;
     try{
         const user = await AdminModel.find({email});
@@ -73,8 +74,56 @@ app.post("/login",async(req,res)=>{
     }
 })
 
-app.use(authenticate);
-app.use("/hotels", hotelRouter)
+app.post("/user/signup", async(req,res)=>{
+    const {name,email,password,phone} = req.body;
+    const userpresent = await UserModel.findOne({email})
+
+    if(userpresent?.email){
+        res.send("signup with new email")
+    }
+    else{
+        try{
+            bcrypt.hash(password, 5, async function(err, hash) {
+                const user = new UserModel({name,email,password:hash,phone})
+                await user.save();
+                res.send("signup successful")
+            });
+        }
+        catch(err){
+            res.send(err)
+        }
+    }
+
+})
+
+app.post("/user/login",async(req,res)=>{
+    const {email,password}=req.body;
+    try{
+        const user = await UserModel.find({email});
+        if(user.length > 0){
+            const hash_pass = user[0].password; 
+            bcrypt.compare(password, hash_pass, function(err, result) {
+                //console.log(result)
+                if(result){
+                    const token = jwt.sign({"userid":user[0]._id}, 'shhhhh');
+                    res.send({"msg":"Login successfull","token" : token})
+                }
+                else{
+                    res.send("login failed")
+                }
+            });
+        }
+        else{
+            res.send("no user")
+        }
+    }
+    catch(err){
+        res.send(err)
+    }
+})
+
+app.use(authenticate_admin);
+app.use("/admin/hotel", hotelRouter)
 
 app.listen(process.env.PORT,async()=>{
     try{
